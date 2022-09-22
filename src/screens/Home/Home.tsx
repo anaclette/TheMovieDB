b65dcd2d581106278e7e5bec03a8bbf03b34c08b
ana.laclau <ana.laclau@mediamonks.com>
2022-09-22 18:07:43 -0300

    Replaced flatlist with RN snap carousel on Movies component and button with touchables to allow styling them

--- src/screens/Home/Home.tsx
+++ src/screens/Home/Home.tsx
@@ -1,18 +1,19 @@
 import React from 'react';
 import {useNavigation} from '@react-navigation/core';
-import {Button, SafeAreaView, Text} from 'react-native';
+import {TouchableOpacity, SafeAreaView, Text} from 'react-native';
 import {styles} from './home.style';
 
 export const Home = () => {
   const navigation = useNavigation();
   return (
-    <SafeAreaView>
+    <SafeAreaView style={styles.safeAreaView}>
       <Text style={styles.title}>Home</Text>
 
-      <Button
-        title="Check out movies"
-        onPress={() => navigation.navigate('Movies')}
-      />
+      <TouchableOpacity
+        style={styles.navigateButton}
+        onPress={() => navigation.navigate('Movies')}>
+        <Text style={styles.buttonText}>Check out movies</Text>
+      </TouchableOpacity>
     </SafeAreaView>
   );
 };
--- src/screens/Movies/Movies.tsx
+++ src/screens/Movies/Movies.tsx
@@ -3,23 +3,30 @@ import React, {useCallback} from 'react';
 import {
   Text,
   SafeAreaView,
-  Button,
-  FlatList,
+  TouchableOpacity,
   View,
   ActivityIndicator,
+  Dimensions,
 } from 'react-native';
 import {useMovies} from '../../hooks/useMovies';
+import Carousel from 'react-native-snap-carousel';
 import {Movie} from '../../types/types';
 import {styles} from './movies.style';
 import MovieCard from '../../components/MovieCard';
+import {useSafeAreaInsets} from 'react-native-safe-area-context';
+import metrics from '../../themes/metrics';
+
+export const SLIDER_WIDTH = Dimensions.get('window').width + metrics.scale(70);
+export const ITEM_WIDTH = Math.round(SLIDER_WIDTH * 0.7);
 
 export const Movies = () => {
+  const {top} = useSafeAreaInsets();
   const navigation = useNavigation();
   const {nowPlaying, isLoading} = useMovies();
 
   const renderItem = useCallback(({item}: {item: Movie}) => {
     return (
-      <View>
+      <View style={styles.carousel}>
         <MovieCard movie={item} />
       </View>
     );
@@ -32,14 +39,20 @@ export const Movies = () => {
           <ActivityIndicator color="red" size={100} />
         </View>
       ) : (
-        <SafeAreaView style={styles.moviesContainer}>
+        <SafeAreaView style={[styles.safeAreaView, {paddingTop: top + 20}]}>
+          <TouchableOpacity
+            style={styles.backButton}
+            onPress={() => navigation.navigate('Home')}>
+            <Text style={styles.buttonText}>Back</Text>
+          </TouchableOpacity>
           <Text style={styles.title}>Now playing movies</Text>
-          <Button
-            title="Back to Home"
-            onPress={() => navigation.navigate('Home')}
+          <Carousel
+            vertical={false}
+            data={nowPlaying as Movie[]}
+            renderItem={renderItem}
+            sliderWidth={SLIDER_WIDTH}
+            itemWidth={ITEM_WIDTH}
           />
-
-          <FlatList data={nowPlaying} numColumns={2} renderItem={renderItem} />
         </SafeAreaView>
       )}
     </>
