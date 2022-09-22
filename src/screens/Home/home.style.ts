034960f3b79150bbd47cd292ac13ec696bfb62cd
ana.laclau <ana.laclau@mediamonks.com>
2022-09-22 18:06:25 -0300

    Improved button styling

--- src/screens/Home/home.style.ts
+++ src/screens/Home/home.style.ts
@@ -1,8 +1,27 @@
 import {StyleSheet} from 'react-native';
+import colors from '../../themes/colors';
 import fonts from '../../themes/fonts';
+import metrics from '../../themes/metrics';
 
 export const styles = StyleSheet.create({
+  safeAreaView: {
+    flex: 1,
+  },
   title: {
     ...fonts.Amatic,
+  },
+  navigateButton: {
+    borderBottomColor: colors.darkPink,
+    borderBottomWidth: 1,
+    borderLeftWidth: 1,
+    borderRadius: metrics.scale(3),
+    borderLeftColor: colors.darkPink,
+    alignSelf: 'flex-end',
+  },
+  buttonText: {
+    paddingHorizontal: metrics.scale(10),
+    color: colors.darkPink,
+    ...fonts.HindSiliguri,
+    fontSize: metrics.scale(20),
   },
 });
--- src/screens/Movies/movies.style.ts
+++ src/screens/Movies/movies.style.ts
@@ -1,11 +1,16 @@
 import {StyleSheet} from 'react-native';
 import colors from '../../themes/colors';
 import fonts from '../../themes/fonts';
+import metrics from '../../themes/metrics';
 
 export const styles = StyleSheet.create({
-  moviesContainer: {
+  safeAreaView: {
     flex: 1,
     justifyContent: 'space-evenly',
+    alignItems: 'center',
+  },
+  carousel: {
+    flex: 1,
     alignItems: 'center',
   },
   loaderContainer: {
@@ -16,5 +21,17 @@ export const styles = StyleSheet.create({
   title: {
     ...fonts.Amatic,
     color: colors.darkPink,
+  },
+  backButton: {
+    alignSelf: 'flex-start',
+    marginLeft: metrics.scale(25),
+    borderColor: colors.wine,
+    borderWidth: 1,
+    borderRadius: metrics.scale(10),
+  },
+  buttonText: {
+    paddingHorizontal: metrics.scale(10),
+    color: colors.wine,
+    ...fonts.HindSiliguri,
   },
 });
