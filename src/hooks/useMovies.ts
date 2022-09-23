72dd39c6ad5c61193802674a62fb8367b7e39ce4
ana.laclau <ana.laclau@mediamonks.com>
2022-09-23 18:28:43 -0300

    Improves useMovies custom hook and left commented out endpoints for future use

--- src/hooks/useMovies.ts
+++ src/hooks/useMovies.ts
@@ -1,15 +1,43 @@
 import {useEffect, useState} from 'react';
 import movies from '../state/api/movieDB';
-import {Movie, MovieDBnowPlaying} from '../types/types';
+import {Movie, MovieDBMoviesResponse} from '../types/types';
+
+interface MoviesState {
+  nowPlaying: Movie[];
+  popular: Movie[];
+  topRated: Movie[];
+  upcoming: Movie[];
+}
 
 export const useMovies = () => {
-  const [nowPlaying, setNowPlaying] = useState<Movie[]>();
   const [isLoading, setIsLoading] = useState(true);
+  const [moviesState, setMoviesState] = useState<MoviesState>({
+    nowPlaying: [],
+    popular: [],
+    topRated: [],
+    upcoming: [],
+  });
 
   const getMovies = async () => {
     try {
-      const response = await movies.get<MovieDBnowPlaying>('/now_playing');
-      setNowPlaying(response.data.results);
+      const nowPlayingPromise =
+        movies.get<MovieDBMoviesResponse>('/now_playing');
+      const popularPromise = movies.get<MovieDBMoviesResponse>('/popular');
+      const topRatedPromise = movies.get<MovieDBMoviesResponse>('/top_rated');
+      const upcomingPromise = movies.get<MovieDBMoviesResponse>('/upcoming');
+      const response = await Promise.all([
+        nowPlayingPromise,
+        popularPromise,
+        topRatedPromise,
+        upcomingPromise,
+      ]);
+
+      setMoviesState({
+        nowPlaying: response[0].data.results,
+        popular: response[1].data.results,
+        topRated: response[2].data.results,
+        upcoming: response[3].data.results,
+      });
       setIsLoading(false);
     } catch (error) {
       // handle error
@@ -21,7 +49,7 @@ export const useMovies = () => {
   }, []);
 
   return {
-    nowPlaying,
+    ...moviesState,
     isLoading,
   };
 };
