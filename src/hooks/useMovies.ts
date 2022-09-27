8fc1fe85aa8dbbed8fc3267b5fcfe12cd4490528
ana.laclau <ana.laclau@mediamonks.com>
2022-09-27 10:40:53 -0300

    Improved useMovies response readability

--- src/hooks/useMovies.ts
+++ src/hooks/useMovies.ts
@@ -33,18 +33,19 @@ export const useMovies = () => {
       const upcomingPromise = movies.get<MovieDBMoviesResponse>(
         `/${ENDPOINTS.UPCOMING}`,
       );
-      const response = await Promise.all([
-        nowPlayingPromise,
-        popularPromise,
-        topRatedPromise,
-        upcomingPromise,
-      ]);
+      const [nowPlayingResp, popularResp, topRatedResp, upcomingResp] =
+        await Promise.all([
+          nowPlayingPromise,
+          popularPromise,
+          topRatedPromise,
+          upcomingPromise,
+        ]);
 
       setMoviesState({
-        nowPlaying: response[0].data.results,
-        popular: response[1].data.results,
-        topRated: response[2].data.results,
-        upcoming: response[3].data.results,
+        nowPlaying: nowPlayingResp.data.results,
+        popular: popularResp.data.results,
+        topRated: topRatedResp.data.results,
+        upcoming: upcomingResp.data.results,
       });
       setIsLoading(false);
     } catch (error) {
