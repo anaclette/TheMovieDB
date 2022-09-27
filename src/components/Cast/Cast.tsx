ee5e1bfec45c847303ab6c4225d84afde42fbf83
ana.laclau <ana.laclau@mediamonks.com>
2022-09-27 11:20:18 -0300

    Localized Cast title and added key to children

--- src/components/Cast/Cast.tsx
+++ src/components/Cast/Cast.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import {Text, TouchableOpacity, View} from 'react-native';
 import {CastResp} from '../../types/creditsInterface';
+import copies from '../../utils/copies';
 import {styles} from './cast.style';
 
 interface Props {
@@ -10,11 +11,11 @@ interface Props {
 export const Cast = ({cast}: Props) => {
   return (
     <>
-      <Text style={styles.castTitle}>Cast </Text>
+      <Text style={styles.castTitle}>{copies.es.detailTitle.cast} </Text>
       <View style={styles.castWrapper}>
-        {cast.map(member => {
+        {cast.map((member, index) => {
           return (
-            <TouchableOpacity>
+            <TouchableOpacity key={index}>
               <Text style={styles.castMember}>{member.name}</Text>
             </TouchableOpacity>
           );
