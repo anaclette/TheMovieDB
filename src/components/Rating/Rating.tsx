98d008e5aeed5a573beee49fb0edc3f75f945b1e
ana.laclau <ana.laclau@mediamonks.com>
2022-09-27 10:30:02 -0300

    Created proper Rating component

--- src/components/Rating/Rating.tsx
+++ src/components/Rating/Rating.tsx
@@ -4,10 +4,38 @@ import colors from '../../themes/colors';
 import Icon from 'react-native-vector-icons/Ionicons';
 import {styles} from './rating.style';
 
-export const Rating = () => {
-  return (
-    <View style={styles.container}>
-      <Icon name="star-outline" size={20} color={colors.blackChocolate} />
-    </View>
-  );
+interface Props {
+  rating: number;
+}
+
+export const Rating = ({rating}: Props) => {
+  const ratingStars = (ratingValue: number) => {
+    const maxRate = 5;
+    const fullStar = Math.floor(ratingValue / 2);
+    const halfStar = (ratingValue / 2) % 1 !== 0;
+    const rate = [];
+
+    for (let i = 0; i < fullStar; i++) {
+      rate.push(<Icon name="star" size={20} color={colors.blackChocolate} />);
+    }
+
+    if (halfStar) {
+      rate.push(
+        <Icon name="star-half" size={20} color={colors.blackChocolate} />,
+      );
+    }
+
+    if (rate.length < maxRate) {
+      const rest = maxRate - rate.length;
+      for (let j = 0; j < rest; j++) {
+        rate.push(
+          <Icon name="star-outline" size={20} color={colors.blackChocolate} />,
+        );
+      }
+    }
+
+    return <View style={styles.container}>{rate}</View>;
+  };
+
+  return ratingStars(rating);
 };
--- src/components/Rating/rating.style.ts
+++ src/components/Rating/rating.style.ts
@@ -4,7 +4,8 @@ import metrics from '../../themes/metrics';
 export const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
     height: metrics.scaleVertical(50),
+    flexDirection: 'row',
+    alignItems: 'center',
   },
 });
