4472631c65821ace70d27050a3e1aa654e560796
ana.laclau <ana.laclau@mediamonks.com>
2022-10-03 13:23:18 -0300

    Changed splash screen background color in both android and iOS platforms

--- ios/MoviesApp/AppDelegate.mm
+++ ios/MoviesApp/AppDelegate.mm
@@ -46,11 +46,7 @@ static NSString *const kRNConcurrentRoot = @"concurrentRoot";
   NSDictionary *initProps = [self prepareInitialProps];
   UIView *rootView = RCTAppSetupDefaultRootView(bridge, @"MoviesApp", initProps);
 
-  if (@available(iOS 13.0, *)) {
-    rootView.backgroundColor = [UIColor systemBackgroundColor];
-  } else {
-    rootView.backgroundColor = [UIColor whiteColor];
-  }
+ rootView.backgroundColor = [UIColor colorWithRed:0.76078 green:0.89020 blue:1.00000 alpha:1.00000];
 
   self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
   UIViewController *rootViewController = [UIViewController new];
