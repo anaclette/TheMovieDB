f3bcc9b4d8a1288acf29dce6f6edb022b303ef6d
ana.laclau <ana.laclau@mediamonks.com>
2022-10-11 11:12:25 -0300

    Added TV interfaces

--- src/types/tvInterface.ts
+++ src/types/tvInterface.ts
@@ -22,3 +22,85 @@ export interface TvDetails {
   vote_average: number;
   vote_count: number;
 }
+export interface FullTvDetails {
+  adult: boolean;
+  backdrop_path: string;
+  created_by: any[];
+  episode_run_time: number[];
+  first_air_date: string;
+  genres: Genre[];
+  homepage: string;
+  id: number;
+  in_production: boolean;
+  languages: string[];
+  last_air_date: string;
+  last_episode_to_air: TEpisodeToAir;
+  name: string;
+  next_episode_to_air: TEpisodeToAir;
+  networks: Network[];
+  number_of_episodes: number;
+  number_of_seasons: number;
+  origin_country: string[];
+  original_language: string;
+  original_name: string;
+  overview: string;
+  popularity: number;
+  poster_path: string;
+  production_companies: Network[];
+  production_countries: ProductionCountry[];
+  seasons: Season[];
+  spoken_languages: SpokenLanguage[];
+  status: string;
+  tagline: string;
+  type: string;
+  vote_average: number;
+  vote_count: number;
+}
+
+export interface Genre {
+  id: number;
+  name: string;
+}
+
+export interface TEpisodeToAir {
+  air_date: string;
+  episode_number: number;
+  id: number;
+  name: string;
+  overview: string;
+  production_code: string;
+  runtime: null;
+  season_number: number;
+  show_id: number;
+  still_path: null;
+  vote_average: number;
+  vote_count: number;
+}
+
+export interface Network {
+  id: number;
+  name: string;
+  logo_path: null | string;
+  origin_country: string;
+}
+
+export interface ProductionCountry {
+  iso_3166_1: string;
+  name: string;
+}
+
+export interface Season {
+  air_date: string;
+  episode_count: number;
+  id: number;
+  name: string;
+  overview: string;
+  poster_path: null;
+  season_number: number;
+}
+
+export interface SpokenLanguage {
+  english_name: string;
+  iso_639_1: string;
+  name: string;
+}
