6e6427bd0e4b3704fca3e37bf70bc572f52aa0b5
ana.laclau <ana.laclau@mediamonks.com>
2022-11-03 15:08:28 -0300

    Fixed i18n translation copies not working by wrapping app with i18next provider

--- App.tsx
+++ App.tsx
@@ -5,15 +5,19 @@ import NavigationController from './src/navigation/NavigationController';
 import SplashScreen from './src/components/SplashScreen';
 import {Provider} from 'react-redux';
 import {store} from './src/state/store';
+import {I18nextProvider} from 'react-i18next';
+import i18n from './src/locale/config';
 
 const App = () => {
   return (
     <Provider store={store}>
-      <SplashScreen>
-        <NavigationContainer>
-          <NavigationController />
-        </NavigationContainer>
-      </SplashScreen>
+      <I18nextProvider i18n={i18n}>
+        <SplashScreen>
+          <NavigationContainer>
+            <NavigationController />
+          </NavigationContainer>
+        </SplashScreen>
+      </I18nextProvider>
     </Provider>
   );
 };
