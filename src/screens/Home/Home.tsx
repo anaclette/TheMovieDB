f84133e6d3a64216d5cf687fd915e631b356de45
ana.laclau <ana.laclau@mediamonks.com>
2022-11-03 15:38:27 -0300

    Removed no longer used code in Home component that prevented from compiling

--- src/screens/Home/Home.tsx
+++ src/screens/Home/Home.tsx
@@ -1,48 +1,8 @@
 import React from 'react';
-import {useNavigation} from '@react-navigation/core';
-import {TouchableOpacity, SafeAreaView, Text, View} from 'react-native';
-import {styles} from './home.style';
-import copies from '../../utils/copies';
-import Icon from 'react-native-vector-icons/Ionicons';
-import metrics from '../../themes/metrics';
-import colors from '../../themes/colors';
 
-type Navigation = {
-  navigate: (screen: string) => void;
-};
+import {SafeAreaView} from 'react-native';
+import {styles} from './home.style';
 
 export const Home = () => {
-  const navigation = useNavigation<Navigation>();
-  return (
-    <SafeAreaView style={styles.safeAreaView}>
-      <TouchableOpacity
-        style={styles.navigateButton}
-        onPress={() => navigation.navigate('Movies')}>
-        <View style={styles.buttonContentWrapper}>
-          <Icon
-            name="videocam-outline"
-            size={metrics.scale(30)}
-            color={colors.brown}
-          />
-          <Text style={styles.buttonText}>
-            {copies.es.movies.navTitle.movies}
-          </Text>
-        </View>
-      </TouchableOpacity>
-      <TouchableOpacity
-        style={styles.navigateButton}
-        onPress={() => navigation.navigate('Tv')}>
-        <View style={styles.buttonContentWrapper}>
-          <Icon
-            name="tv-outline"
-            size={metrics.scale(30)}
-            color={colors.brown}
-          />
-          <Text style={styles.buttonText}>
-            {copies.es.movies.navTitle.series}
-          </Text>
-        </View>
-      </TouchableOpacity>
-    </SafeAreaView>
-  );
+  return <SafeAreaView style={styles.safeAreaView}></SafeAreaView>;
 };
