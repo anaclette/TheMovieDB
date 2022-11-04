2bdd02e7d9cbbbfff725a456d87b51db9485d542
ana.laclau <ana.laclau@mediamonks.com>
2022-11-04 12:06:00 -0300

    Added missing persistor to App PersistGate wrapper

--- App.tsx
+++ App.tsx
@@ -4,20 +4,23 @@ import {NavigationContainer} from '@react-navigation/native';
 import NavigationController from './src/navigation/NavigationController';
 import SplashScreen from './src/components/SplashScreen';
 import {Provider} from 'react-redux';
-import {store} from './src/state/store';
+import {store, persistor} from './src/state/store';
 import {I18nextProvider} from 'react-i18next';
 import i18n from './src/locale/config';
+import {PersistGate} from 'redux-persist/lib/integration/react';
 
 const App = () => {
   return (
     <Provider store={store}>
-      <I18nextProvider i18n={i18n}>
-        <SplashScreen>
-          <NavigationContainer>
-            <NavigationController />
-          </NavigationContainer>
-        </SplashScreen>
-      </I18nextProvider>
+      <PersistGate persistor={persistor}>
+        <I18nextProvider i18n={i18n}>
+          <SplashScreen>
+            <NavigationContainer>
+              <NavigationController />
+            </NavigationContainer>
+          </SplashScreen>
+        </I18nextProvider>
+      </PersistGate>
     </Provider>
   );
 };
