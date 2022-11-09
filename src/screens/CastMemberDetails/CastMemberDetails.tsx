eeefece7fc14cef5add71d71dd464b331bde1e4f
ana.laclau <ana.laclau@mediamonks.com>
2022-11-09 15:24:18 -0300

    Polished CastMemberDetails component structure

--- src/screens/CastMemberDetails/CastMemberDetails.tsx
+++ src/screens/CastMemberDetails/CastMemberDetails.tsx
@@ -1,16 +1,12 @@
 import {NativeStackScreenProps} from '@react-navigation/native-stack';
 import React from 'react';
-import {
-  FlatList,
-  Image,
-  ScrollView,
-  Text,
-  View,
-  SafeAreaView,
-} from 'react-native';
+import {useTranslation} from 'react-i18next';
+import {Image, ScrollView, Text, View, SafeAreaView} from 'react-native';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 import {imageURL} from '../../common/constants';
 import Button from '../../components/Button';
+import Loader from '../../components/Loader';
+import {TranslationKeys} from '../../locale/translations/keys';
 import {RootStackParamList} from '../../navigation/NavigationController';
 import {
   useGetCombinedCreditsQuery,
@@ -18,93 +14,119 @@ import {
 } from '../../state/cast';
 import colors from '../../themes/colors';
 import metrics from '../../themes/metrics';
-import {CombinedCreditsCast} from '../../types/castMemberInterface';
+import {getYear} from '../../utils/helpers';
 import {styles} from './castMemberDetails.style';
 
 type Props = NativeStackScreenProps<RootStackParamList, 'CastMemberDetails'>;
 
 export const CastMemberDetails = ({route, navigation}: Props) => {
+  const {t} = useTranslation();
   const params = route.params;
   const {data: castMemberData} = useGetMemberDetailsQuery(params.id);
-  const {data: castMemberCombinedCredits} = useGetCombinedCreditsQuery(
-    params.id,
-  );
-
-  const renderItem = ({item}: {item: CombinedCreditsCast}) => {
-    const isMovie = item.media_type === 'movie';
-
-    return (
-      <Button
-        onPress={() =>
-          navigation.navigate(isMovie ? 'MovieDetails' : 'TvDetails', item)
-        }
-        children={
-          <View
-            style={{
-              borderWidth: 2,
-              marginVertical: 20,
-              borderColor: 'black',
-              width: 200,
-            }}>
-            <Image
-              source={
-                item.poster_path
-                  ? {uri: `${imageURL}${item.poster_path}`}
-                  : require('../../assets/images/No-img-available.png')
-              }
-              style={{width: '100%', height: 300}}
-            />
-            <Icon
-              name={isMovie ? 'movie' : 'tv'}
-              size={metrics.scale(20)}
-              color={colors.petroleum}
-            />
-            {item.name && <Text>Nombre: {item.name}</Text>}
-            {item.overview && (
-              <Text numberOfLines={3}>Sinopsis: {item.overview}</Text>
-            )}
-            {item.release_date && (
-              <Text>Fecha de lanzamiento: {item.release_date}</Text>
-            )}
-            {item.title && <Text>Título: {item.title}</Text>}
-          </View>
-        }
-      />
-    );
-  };
+  const {data: castMemberCombinedCredits, isLoading} =
+    useGetCombinedCreditsQuery(params.id);
 
   return (
-    <SafeAreaView>
+    <SafeAreaView style={styles.safeAreaView}>
       <ScrollView>
-        <View style={styles.nameAndImageWrapper}>
-          <Text>{castMemberData?.name}</Text>
-          <View style={styles.imageWrapper}>
+        <View style={styles.personalInfoWrapper}>
+          <View style={styles.personalInfoDetails}>
+            <Text style={styles.name}>{castMemberData?.name}</Text>
+            {castMemberData?.birthday && (
+              <Text style={styles.secondaryDetailsText}>
+                {castMemberData?.birthday}
+              </Text>
+            )}
+            {castMemberData?.place_of_birth && (
+              <Text style={styles.secondaryDetailsText}>
+                {castMemberData?.place_of_birth}
+              </Text>
+            )}
+          </View>
+
+          <View style={styles.profileImageWrapper}>
             <Image
               source={{uri: `${imageURL}${castMemberData?.profile_path}`}}
-              style={styles.image}
+              style={[styles.image, styles.profileImage]}
             />
           </View>
         </View>
 
-        {castMemberData?.birthday && <Text>{castMemberData?.birthday}</Text>}
-        {castMemberData?.place_of_birth && (
-          <Text>{castMemberData?.place_of_birth}</Text>
-        )}
+        <View style={styles.biographyWrapper}>
+          {castMemberData?.biography ? (
+            <Text style={styles.biography}>{castMemberData?.biography}</Text>
+          ) : (
+            <Text style={styles.biography}>
+              {t(TranslationKeys.NO_FURTHER_INFO)}
+            </Text>
+          )}
+          <Text style={styles.otherCreditsTitle}>
+            {t(TranslationKeys.ALSO_IN)}
+          </Text>
+        </View>
 
-        {castMemberData?.biography ? (
-          <Text>{castMemberData?.biography}</Text>
+        {isLoading ? (
+          <Loader />
         ) : (
-          <Text>No hay mayor información.</Text>
+          <View style={styles.combinedCreditsWrapper}>
+            {castMemberCombinedCredits?.map((item, index) => {
+              const isMovie = item.media_type === 'movie';
+              return (
+                <Button
+                  key={index}
+                  wrapperStyle={styles.cardWrappingButton}
+                  onPress={() =>
+                    navigation.navigate(
+                      isMovie ? 'MovieDetails' : 'TvDetails',
+                      item,
+                    )
+                  }
+                  children={
+                    <View>
+                      <View style={styles.combinedCreditsImageWrapper}>
+                        <Image
+                          source={
+                            item.poster_path
+                              ? {uri: `${imageURL}${item.poster_path}`}
+                              : require('../../assets/images/No-img-available.png')
+                          }
+                          style={[styles.image, styles.combinedCreditsImage]}
+                        />
+                      </View>
+
+                      <View style={styles.creditDetailsWrapper}>
+                        <View style={styles.creditDetailsContainer}>
+                          {item.title && (
+                            <Text style={[styles.detail, styles.creditTitle]}>
+                              {item.title}
+                            </Text>
+                          )}
+                          {item.name && (
+                            <Text style={[styles.detail, styles.creditTitle]}>
+                              {item.name}
+                            </Text>
+                          )}
+                          {item.release_date && (
+                            <Text style={styles.detail}>
+                              ({getYear(item.release_date)})
+                            </Text>
+                          )}
+                        </View>
+
+                        <Icon
+                          name={isMovie ? 'movie' : 'tv'}
+                          size={metrics.scale(20)}
+                          color={colors.lightBlue}
+                        />
+                      </View>
+                    </View>
+                  }
+                />
+              );
+            })}
+          </View>
         )}
       </ScrollView>
-
-      <Text>También actuó en: </Text>
-      <FlatList
-        numColumns={2}
-        data={castMemberCombinedCredits}
-        keyExtractor={(_, index) => index.toString()}
-        renderItem={renderItem}
-      />
     </SafeAreaView>
   );
 };
