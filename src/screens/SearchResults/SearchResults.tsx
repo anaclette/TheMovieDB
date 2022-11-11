4e2da6fab019e71602cde24ba4099520425da719
ana.laclau <ana.laclau@mediamonks.com>
2022-11-11 18:16:14 -0300

    Worked on SearchResults layout and performance

--- src/screens/SearchResults/SearchResults.tsx
+++ src/screens/SearchResults/SearchResults.tsx
@@ -1,21 +1,27 @@
 import {StackScreenProps} from '@react-navigation/stack';
-import React, {useState} from 'react';
+import React, {useRef, useState} from 'react';
+import debounce from 'lodash.debounce';
 import {
   Text,
-  ScrollView,
   View,
   Keyboard,
   KeyboardAvoidingView,
   Platform,
   SafeAreaView,
   TextInput,
+  FlatList,
+  Image,
 } from 'react-native';
 import {RootStackParamList} from '../../navigation/NavigationController';
 import colors from '../../themes/colors';
 import metrics from '../../themes/metrics';
 import {styles} from './searchResults.style';
+
 import Button from '../../components/Button';
 import {useGetSearchResultQuery} from '../../state/search';
+import {SearchResult} from '../../types/multiSearch';
+import {imageURL} from '../../common/constants';
+import CombinedCreditsCard from '../../components/CombinedCreditsCard';
 
 export const SearchResults = ({
   navigation,
@@ -25,6 +31,51 @@ export const SearchResults = ({
     keyword: userInput,
     page: 1,
   });
+
+  const debouncedSave = useRef(
+    debounce(input => {
+      setUserInput(input);
+    }, 1000),
+  ).current;
+
+  const renderItem = ({item, index}: {item: SearchResult; index: number}) => {
+    const isPerson = item.media_type === 'person';
+    const isTv = item.media_type === 'tv';
+
+    return !isPerson ? (
+      <Button
+        onPress={() =>
+          navigation.navigate(isTv ? 'TvDetails' : 'MovieDetails', item)
+        }
+        children={
+          <View key={index} style={styles.cardWrapper}>
+            <CombinedCreditsCard item={item} index={index} />
+          </View>
+        }
+      />
+    ) : (
+      <Button
+        onPress={() => navigation.navigate('CastMemberDetails', item)}
+        children={
+          <View key={index} style={styles.cardWrapper}>
+            <View style={styles.imageWrapper}>
+              <Image
+                style={!item.poster_path && styles.noImage}
+                source={
+                  item.poster_path
+                    ? {
+                        uri: `${imageURL}${item.poster_path}`,
+                      }
+                    : require('../../assets/images/No-img-available.png')
+                }
+              />
+            </View>
+            <Text style={styles.title}>{item.name}</Text>
+          </View>
+        }
+      />
+    );
+  };
 
   return (
     <SafeAreaView>
@@ -54,9 +105,7 @@ export const SearchResults = ({
               ]}
               placeholder="Encuentra"
               placeholderTextColor={colors.petroleum}
-              defaultValue={userInput}
-              onChangeText={input => setUserInput(input)}
-              value={userInput}
+              onChangeText={debouncedSave}
               onSubmitEditing={input => setUserInput(input.nativeEvent.text)}
               autoFocus={true}
               autoCorrect={false}
@@ -73,20 +122,14 @@ export const SearchResults = ({
           </View>
         </View>
       </KeyboardAvoidingView>
-
-      <ScrollView>
-        {!!userInput &&
-          searchResults?.map((item, index) => (
-            <View
-              key={index}
-              style={{borderWidth: 2, borderColor: 'white', margin: 5}}>
-              {item.name && <Text style={{color: 'white'}}>{item.name}</Text>}
-              {item.title && <Text style={{color: 'white'}}>{item.title}</Text>}
-              <Text style={{color: 'white'}}>{item.media_type}</Text>
-              <Text style={{color: 'white'}}>{item.overview}</Text>
-            </View>
-          ))}
-      </ScrollView>
+      {!!userInput && (
+        <FlatList
+          numColumns={2}
+          data={searchResults}
+          renderItem={renderItem}
+          keyExtractor={(_, index) => index.toString()}
+        />
+      )}
     </SafeAreaView>
   );
 };
--- src/screens/SearchResults/searchResults.style.ts
+++ src/screens/SearchResults/searchResults.style.ts
@@ -13,6 +13,7 @@ export const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'space-around',
     alignItems: 'center',
+    marginVertical: metrics.scale(10),
   },
   inputWrapper: {
     flexDirection: 'row',
@@ -30,5 +31,31 @@ export const styles = StyleSheet.create({
   },
   backButton: {
     left: metrics.scale(8),
+  },
+
+  cardWrapper: {
+    flex: 1,
+    width: metrics.screenWidth / 2 - metrics.scale(5),
+    marginHorizontal: metrics.scale(2),
+    marginVertical: metrics.scale(10),
+  },
+  imageWrapper: {
+    borderRadius: metrics.scale(20),
+    height: metrics.scale(240),
+  },
+  noImage: {
+    flex: 1,
+    width: '100%',
+    borderRadius: metrics.scale(20),
+  },
+  detail: {
+    color: colors.palePink,
+    ...fonts.HindSiliguri,
+  },
+  title: {
+    maxWidth: '95%',
+    fontWeight: 'bold',
+    color: colors.palePink,
+    ...fonts.HindSiliguri,
   },
 });
