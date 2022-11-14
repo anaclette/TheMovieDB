657ba332099353adf23b924e1ad196f414cab083
ana.laclau <ana.laclau@mediamonks.com>
2022-11-14 11:47:05 -0300

    Rendered MovieCard and TvCard in FullCategoryContent component

--- src/screens/FullCategoryContent/FullCategoryContent.tsx
+++ src/screens/FullCategoryContent/FullCategoryContent.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
-import {SafeAreaView, Text, View} from 'react-native';
+import {ScrollView, Text, View} from 'react-native';
 import Button from '../../components/Button';
 import {StackScreenProps} from '@react-navigation/stack';
 import {RootStackParamList} from '../../navigation/NavigationController';
 import colors from '../../themes/colors';
 import metrics from '../../themes/metrics';
 import {styles} from './fullCategoryContent.style';
+import MovieCard from '../../components/MovieCard';
+import Rating from '../../components/Rating';
+import TvCard from '../../components/TvCard';
 
 interface Props
   extends StackScreenProps<RootStackParamList, 'FullCategoryContent'> {}
@@ -14,8 +17,9 @@ export const FullCategoryContent = ({route, navigation}: Props) => {
   const movie = route.params.movie;
   const tvShow = route.params.tvShow;
   // const page = route.params.page;
+
   return (
-    <SafeAreaView>
+    <>
       <Button
         size={metrics.scale(20)}
         wrapperStyle={styles.backButton}
@@ -23,18 +27,26 @@ export const FullCategoryContent = ({route, navigation}: Props) => {
         onPress={() => navigation.goBack()}
         color={colors.palePink}
       />
-      {movie &&
-        movie?.map((item, index) => (
-          <View key={index}>
-            {item.title && <Text style={{color: 'white'}}>{item.title}</Text>}
-          </View>
-        ))}
-      {tvShow &&
-        tvShow?.map((item, index) => (
-          <View key={index}>
-            <Text style={{color: 'white'}}>{item.name}</Text>
-          </View>
-        ))}
-    </SafeAreaView>
+      <ScrollView>
+        <View style={styles.container}>
+          {movie &&
+            movie?.map((item, index) => (
+              <View key={index} style={styles.innerContainer}>
+                <Text style={styles.title}>{item.title}</Text>
+                <View style={styles.cardWrapper}>
+                  <MovieCard key={index} movie={item} />
+                </View>
+                <Rating rating={item.vote_average} color={colors.lightBlue} />
+              </View>
+            ))}
+          {tvShow &&
+            tvShow?.map((item, index) => (
+              <View key={index} style={styles.tvShowsInnerContainer}>
+                <TvCard isFullContentPage item={item} />
+              </View>
+            ))}
+        </View>
+      </ScrollView>
+    </>
   );
 };
--- src/screens/FullCategoryContent/fullCategoryContent.style.ts
+++ src/screens/FullCategoryContent/fullCategoryContent.style.ts
@@ -1,9 +1,37 @@
 import {StyleSheet} from 'react-native';
-// import colors from '../../themes/colors';
+import colors from '../../themes/colors';
+import fonts from '../../themes/fonts';
 import metrics from '../../themes/metrics';
 
 export const styles = StyleSheet.create({
   backButton: {
     left: metrics.scale(5),
+  },
+  container: {
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+  },
+  title: {
+    ...fonts.InconsolataMedium,
+    color: colors.palePink,
+    alignSelf: 'center',
+    textAlign: 'center',
+    flex: 1,
+    flexGrow: 20,
+    marginVertical: metrics.scale(5),
+    fontSize: metrics.scaledFontSize(18),
+  },
+  cardWrapper: {
+    flex: 1,
+    justifyContent: 'center',
+    height: metrics.screenHeight / 2.8,
+  },
+  innerContainer: {
+    alignItems: 'center',
+    marginVertical: metrics.scale(5),
+    width: '50%',
+  },
+  tvShowsInnerContainer: {
+    width: '50%',
   },
 });
