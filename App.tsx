705f082fc3a7b8174526b1237534fed0981cd58f
ana.laclau <ana.laclau@mediamonks.com>
2022-11-17 10:40:51 -0300

    Removed unnecessary i18n provider wrapper in App file

--- App.tsx
+++ App.tsx
@@ -5,21 +5,17 @@ import NavigationController from './src/navigation/NavigationController';
 import SplashScreen from './src/components/SplashScreen';
 import {Provider} from 'react-redux';
 import {store, persistor} from './src/state/store';
-import {I18nextProvider} from 'react-i18next';
-import i18n from './src/locale/config';
 import {PersistGate} from 'redux-persist/lib/integration/react';
 
 const App = () => {
   return (
     <Provider store={store}>
       <PersistGate persistor={persistor}>
-        <I18nextProvider i18n={i18n}>
-          <SplashScreen>
-            <NavigationContainer>
-              <NavigationController />
-            </NavigationContainer>
-          </SplashScreen>
-        </I18nextProvider>
+        <SplashScreen>
+          <NavigationContainer>
+            <NavigationController />
+          </NavigationContainer>
+        </SplashScreen>
       </PersistGate>
     </Provider>
   );
