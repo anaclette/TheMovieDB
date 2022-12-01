51dbbf21d07cbb3dd7baef23a605b3df055957a4
ana.laclau <ana.laclau@mediamonks.com>
2022-12-01 12:17:28 -0300

    Fixed disabled value in FullCategoryContent next page button

--- src/screens/FullCategoryContent/FullCategoryContent.tsx
+++ src/screens/FullCategoryContent/FullCategoryContent.tsx
@@ -160,7 +160,7 @@ export const FullCategoryContent = ({route, navigation}: Props) => {
               text={t(TranslationKeys.SHOW_NEXT_PAGE)}
               onPress={loadNextPage}
               textStyle={styles.buttonText}
-              disabled={pageNumber === data!.length - 20}
+              disabled={!(data!.length + 20)}
             />
           </View>
         </>
